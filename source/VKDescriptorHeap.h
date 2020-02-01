a6e900e00f758344bcc5a4e0dee699ae4a873df3
BearIvan <i-sobolevskiy@mail.ru>
2020-02-01 11:23:17 +0700

    * UPG:DescriptorHeap

--- source/VKDescriptorHeap.h
+++ source/VKDescriptorHeap.h
@@ -6,8 +6,17 @@ public:
 	VKDescriptorHeap(const BearDescriptorHeapDescription& desc);
 	virtual ~VKDescriptorHeap();
 	void Set(VkCommandBuffer  CommandLine);
+	virtual void SetUniformBuffer(bsize slot, BearFactoryPointer<BearRHI::BearRHIUniformBuffer> UniformBuffer);
+	virtual	void SetShaderResource(bsize slot, BearFactoryPointer<BearRHI::BearRHIShaderResource> ShaderResource);
+	virtual	void SetSampler(bsize slot, BearFactoryPointer<BearRHI::BearRHISampler> Sampler);
 	VkDescriptorSet DescriptorSet;
 	BearFactoryPointer<BearRHI::BearRHIRootSignature> RootSignature;
 	VKRootSignature* RootSignaturePointer;
+	VkDescriptorPool DescriptorPool;
+	BearFactoryPointer<BearRHI::BearRHIUniformBuffer> UniformBuffers[16];
 	bsize CountBuffers;
+	BearFactoryPointer<BearRHI::BearRHIShaderResource> ShaderResources[16];
+	bsize CountSRVs;
+	BearFactoryPointer<BearRHI::BearRHISampler> Samplers[16];
+	bsize CountSamplers;
 };
